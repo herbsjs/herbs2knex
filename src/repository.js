2ce7490edc0dc88a976008cf6c378a7d34add047
Enderson Costa <enderson.tech@gmail.com>
2020-12-28 19:23:51 -0300

    eslint fix

--- src/repository.js
+++ src/repository.js
@@ -1,64 +1,64 @@
-const Convention = require("./convention");
-const DataMapper = require("./dataMapper");
-const Knex = require("knex");
+const Convention = require("./convention")
+const DataMapper = require("./dataMapper")
+const Knex = require("knex")
 
-const dependency = { convention: Convention, knex: Knex };
+const dependency = { convention: Convention, knex: Knex }
 
 module.exports = class Repository {
   constructor(options) {
-    const di = Object.assign({}, dependency, options.injection);
-    this.convention = di.convention;
-    this.table = options.table;
-    this.schema = options.schema;
+    const di = Object.assign({}, dependency, options.injection)
+    this.convention = di.convention
+    this.table = options.table
+    this.schema = options.schema
     this.tableQualifiedName = this.schema
       ? `${this.schema}.${this.table}`
-      : `${this.table}`;
-    this.entity = options.entity;
-    this.entityIDs = options.ids;
-    this.run = di.knex(options.dbConfig);
-    this.runner = di.knex(options.dbConfig)(this.tableQualifiedName);
-    this.dataMapper = DataMapper.getFrom(this.entity, this.entityIDs);
+      : `${this.table}`
+    this.entity = options.entity
+    this.entityIDs = options.ids
+    this.run = di.knex(options.dbConfig)
+    this.runner = di.knex(options.dbConfig)(this.tableQualifiedName)
+    this.dataMapper = DataMapper.getFrom(this.entity, this.entityIDs)
   }
 
   mapFields() {
-    const entity = this.entity;
-    this.dataMapper = DataMapper.getFrom(entity);
+    const entity = this.entity
+    this.dataMapper = DataMapper.getFrom(entity)
   }
 
   async findByID(ids) {
-    const dataMapper = this.dataMapper;
-    const tableIDs = dataMapper.getTableIDs();
-    const tableFields = dataMapper.getTableFields();
+    const dataMapper = this.dataMapper
+    const tableIDs = dataMapper.getTableIDs()
+    const tableFields = dataMapper.getTableFields()
 
-    const parsedValue = Array.isArray(ids) ? ids : [ids];
+    const parsedValue = Array.isArray(ids) ? ids : [ids]
     const ret = await this.runner
       .select(tableFields)
-      .whereIn(tableIDs[0], parsedValue);
+      .whereIn(tableIDs[0], parsedValue)
 
-    const entities = [];
+    const entities = []
 
     for (const row of ret) {
-      if (row === undefined) continue;
-      this.dataMapper.load(row);
-      entities.push(this.entity.fromJSON(this.dataMapper));
+      if (row === undefined) continue
+      this.dataMapper.load(row)
+      entities.push(this.entity.fromJSON(this.dataMapper))
     }
 
-    return entities;
+    return entities
   }
 
   async findBy(search) {
-    const dataMapper = this.dataMapper;
-    const tableFields = dataMapper.getTableFields();
+    const dataMapper = this.dataMapper
+    const tableFields = dataMapper.getTableFields()
 
     const searchTermTableField = dataMapper.toTableField(
       Object.keys(search)[0]
-    );
-    const searchTerm = Object.keys(search)[0];
-    if (!searchTerm || searchTerm === "0") throw "search term is invalid";
+    )
+    const searchTerm = Object.keys(search)[0]
+    if (!searchTerm || searchTerm === "0") throw "search term is invalid"
 
     const searchValue = Array.isArray(search[searchTerm])
       ? search[searchTerm]
-      : [search[searchTerm]];
+      : [search[searchTerm]]
 
     if (
       !search[searchTerm] ||
@@ -66,89 +66,89 @@ module.exports = class Repository {
         !Array.isArray(search[searchTerm])) ||
       (Array.isArray(search[searchTerm]) && !search[searchTerm].length)
     )
-      throw "search value is invalid";
+      throw "search value is invalid"
 
     const ret = await this.runner
       .select(tableFields)
-      .whereIn(searchTermTableField, searchValue);
+      .whereIn(searchTermTableField, searchValue)
 
-    const entities = [];
+    const entities = []
 
     for (const row of ret) {
-      if (row === undefined) continue;
-      this.dataMapper.load(row);
-      entities.push(this.entity.fromJSON(this.dataMapper));
+      if (row === undefined) continue
+      this.dataMapper.load(row)
+      entities.push(this.entity.fromJSON(this.dataMapper))
     }
 
-    return entities;
+    return entities
   }
 
   async insert(entityInstance) {
-    const dataMapper = this.dataMapper;
-    const tableFields = dataMapper.getTableFieldsWithValue(entityInstance);
-    await this.runner.insert(tableFields);
-    return true;
+    const dataMapper = this.dataMapper
+    const tableFields = dataMapper.getTableFieldsWithValue(entityInstance)
+    await this.runner.insert(tableFields)
+    return true
   }
 
   async persist(entityInstance) {
-    const updated = await this.update(entityInstance);
-    if (updated) return true;
+    const updated = await this.update(entityInstance)
+    if (updated) return true
 
-    await this.insert(entityInstance);
-    return true;
+    await this.insert(entityInstance)
+    return true
   }
 
   async update(entityInstance) {
-    const dataMapper = this.dataMapper;
-    const tableIDs = dataMapper.getTableIDs();
-    const tableFields = dataMapper.getTableFieldsWithValue(entityInstance);
+    const dataMapper = this.dataMapper
+    const tableIDs = dataMapper.getTableIDs()
+    const tableFields = dataMapper.getTableFieldsWithValue(entityInstance)
 
     const ret = await this.runner
       .where(tableIDs[0], entityInstance[tableIDs[0]])
-      .update(tableFields);
-    return ret === 1;
+      .update(tableFields)
+    return ret === 1
   }
 
   async delete(entityInstance) {
-    const dataMapper = this.dataMapper;
-    const tableIDs = dataMapper.getTableIDs();
+    const dataMapper = this.dataMapper
+    const tableIDs = dataMapper.getTableIDs()
 
     const ret = await this.runner
       .where(tableIDs[0], entityInstance[tableIDs[0]])
-      .delete();
-    return ret === 1;
+      .delete()
+    return ret === 1
   }
 
   async query(sql, values) {
     try {
-      console.info("[SQL]", sql, " [VALUES]", values.toString());
-      const runner = this.run;
-      return await runner.raw(sql, values);
+      console.info("[SQL]", sql, " [VALUES]", values.toString())
+      const runner = this.run
+      return await runner.raw(sql, values)
     } catch (error) {
-      console.error(error);
-      throw error;
+      console.error(error)
+      throw error
     }
   }
 
   async where(params, options) {
-    const dataMapper = this.dataMapper;
+    const dataMapper = this.dataMapper
 
     const preparedParams = Object.keys(params)
       .map((param) => ({ [dataMapper.toTableField(param)]: params[param] }))
-      .reduce((x, y) => ({ ...x, ...y }));
+      .reduce((x, y) => ({ ...x, ...y }))
 
-    let ret;
-    if (options?.first) ret = new Array(await this.runner.where(preparedParams).first());
-    else ret = await this.runner.where(preparedParams);
+    let ret
+    if (options?.first) ret = new Array(await this.runner.where(preparedParams).first())
+    else ret = await this.runner.where(preparedParams)
 
-    const entities = [];
+    const entities = []
 
     for (const row of ret) {
-      if (row === undefined) continue;
-      this.dataMapper.load(row);
-      entities.push(this.entity.fromJSON(this.dataMapper));
+      if (row === undefined) continue
+      this.dataMapper.load(row)
+      entities.push(this.entity.fromJSON(this.dataMapper))
     }
 
-    return entities;
+    return entities
   }
-};
+}
--- test/queries/where.js
+++ test/queries/where.js
@@ -1,6 +1,6 @@
-const { entity, field } = require("gotu");
-const Repository = require("../../src/repository");
-const assert = require("assert");
+const { entity, field } = require("gotu")
+const Repository = require("../../src/repository")
+const assert = require("assert")
 
 describe("Where", () => {
   const givenAnEntity = () => {
@@ -8,58 +8,58 @@ describe("Where", () => {
       id: field(Number),
       stringTest: field(String),
       booleanTest: field(Boolean),
-    });
-  };
+    })
+  }
 
   const givenAnRepositoryClass = (options) => {
     return class ItemRepositoryBase extends Repository {
       constructor(options) {
-        super(options);
+        super(options)
       }
-    };
-  };
+    }
+  }
 
   const returnData = [
     { id: 1, string_test: "john", boolean_test: true },
     { id: 2, string_test: "clare", boolean_test: false },
-  ];
+  ]
 
   const knex = () => {
     return () => ({
       where: (param, options) => {
         return returnData
       },
-    });
-  };
+    })
+  }
 
   it("should return entities", async () => {
     //given
-    const anEntity = givenAnEntity();
-    const injection = { knex };
-    const ItemRepository = givenAnRepositoryClass();
+    const anEntity = givenAnEntity()
+    const injection = { knex }
+    const ItemRepository = givenAnRepositoryClass()
     const itemRepo = new ItemRepository({
       entity: anEntity,
       table: "aTable",
       ids: ["id"],
       dbConfig: {},
       injection,
-    });
+    })
 
     //when
-    const ret = await itemRepo.where({ stringTest: ["john"] });
+    const ret = await itemRepo.where({ stringTest: ["john"] })
 
     //then
     assert.deepStrictEqual(ret[0].toJSON(), {
       id: 1,
       stringTest: "john",
       booleanTest: true,
-    });
+    })
     assert.deepStrictEqual(ret[1].toJSON(), {
       id: 2,
       stringTest: "clare",
       booleanTest: false,
-    });
-  });
+    })
+  })
 
   
   it("should return entities with first", async () => {
@@ -71,29 +71,29 @@ describe("Where", () => {
                 return [returnData[0]]
             },
           }),
-        });
-      };
+        })
+      }
 
     //given
-    const anEntity = givenAnEntity();
-    const injection = { knex: knexInjection };
-    const ItemRepository = givenAnRepositoryClass();
+    const anEntity = givenAnEntity()
+    const injection = { knex: knexInjection }
+    const ItemRepository = givenAnRepositoryClass()
     const itemRepo = new ItemRepository({
       entity: anEntity,
       table: "aTable",
       ids: ["id"],
       dbConfig: {},
       injection,
-    });
+    })
 
     //when
-    const ret = await itemRepo.where({ stringTest: ["john"] }, { first: true });
+    const ret = await itemRepo.where({ stringTest: ["john"] }, { first: true })
 
     //then
     assert.deepStrictEqual(ret[0].toJSON(), {
       id: 1,
       stringTest: "john",
       booleanTest: true,
-    });
-  });
-});
+    })
+  })
+})
