4384453125d96c04e856bb3e82cfd5ceb22b4dc1
David Lojudice Sobrinho <dalssoft@gmail.com>
2023-05-05 12:15:05 -0300

    fix(lint): semicolon

--- src/convention.js
+++ src/convention.js
@@ -3,7 +3,7 @@ module.exports = class Convention {
     return string
       .replace(/^[A-Z]/, (match) => match.toLowerCase()) // Lowercase the first letter
       .replace(/([a-z])([A-Z]+)/g, (match, p1, p2) => p1 + '_' + p2) // Add underscore between lowercase and uppercase letters
-      .toLowerCase(); // Convert the entire string to lowercase
+      .toLowerCase() // Convert the entire string to lowercase
   }
 
   toTableFieldName(entityField) {
